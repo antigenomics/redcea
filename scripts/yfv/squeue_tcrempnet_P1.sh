bf762e44f9a41d054bc2d3c20ac75561e9d152c5
VEK239 <elizaveta.k.vlasova@gmail.com>
2025-07-29 14:43:15 +0300

    updated repo structure

--- scripts/yfv/squeue_tcrempnet_P1.sh
+++ scripts/yfv/squeue_tcrempnet_P1.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 #SBATCH --job-name=tcrempnet_P1       # Job name
-#SBATCH --cpus-per-task=16     # Run on a single CPU
+#SBATCH --cpus-per-task=48     # Run on a single CPU
 #SBATCH --mem=256gb                 # Job memory request
-#SBATCH --time=02:00:00           # Time limit hrs:min:sec
+#SBATCH --time=08:00:00           # Time limit hrs:min:sec
 #SBATCH --output=JobName.%j.log   # Standard output and error log
 #SBATCH --constraint=hpc
-#SBATCH --partition=short
+#SBATCH --partition=medium
 
 
-python tcrEmpNet.py \
+tcrempnet \
   --sample /projects/immunestatus/pogorelyy/airr_format/P1_15_F1.txt \
   --background /projects/immunestatus/pogorelyy/airr_format/P1_0_F1_with_1.txt \
-  --output /projects/immunestatus/pogorelyy/tcrempnet/P1_F1 \
+  --output /projects/immunestatus/pogorelyy/tcrempnet/P1_F1_test \
   --chain TRB \
   --prefix yfv_P1_F1_with_1 \
-  -np 16
+  -np 48
--- scripts/yfv/squeue_tcrempnet_P2.sh
+++ scripts/yfv/squeue_tcrempnet_P2.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 #SBATCH --job-name=tcrempnet_P2        # Job name
-#SBATCH --cpus-per-task=16        # Run on a single CPU
+#SBATCH --cpus-per-task=48        # Run on a single CPU
 #SBATCH --mem=256gb                 # Job memory request
-#SBATCH --time=02:00:00           # Time limit hrs:min:sec
+#SBATCH --time=08:00:00           # Time limit hrs:min:sec
 #SBATCH --output=JobName.%j.log   # Standard output and error log
 #SBATCH --constraint=hpc
-#SBATCH --partition=short
+#SBATCH --partition=medium
 
 
-python tcrEmpNet.py \
+tcrempnet \
   --sample /projects/immunestatus/pogorelyy/airr_format/P2_15_F1.txt \
   --background /projects/immunestatus/pogorelyy/airr_format/P2_0_F1_with_1.txt \
   --output /projects/immunestatus/pogorelyy/tcrempnet/P2_F1 \
   --chain TRB \
   --prefix yfv_P2_F1_with_1 \
-  -np 100
+  -np 48
